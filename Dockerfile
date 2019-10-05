2553190c3f3c6a90574240fb812176a06201c38b
JeanCarloMachado <contato@jeancarlomachado.com.br>
2019-10-05 13:45:23 +0200

    working web

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM python:3.7-stretch
 WORKDIR /app
 COPY . /app
 RUN pip install -r requirements.txt
+EXPOSE 5000
--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def books():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='127.0.0.1')
+    app.run(host='0.0.0.0')

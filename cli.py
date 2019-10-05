f0427d32a5f61620c9856c48aa68ba524768f07c
JeanCarloMachado <contato@jeancarlomachado.com.br>
2019-10-05 14:07:36 +0200

    limiting importing working

--- cli.py
+++ cli.py
@@ -1,13 +1,23 @@
 import click
 import pandas as pd
+from okreads.book import Book
+import json
 
 
 @click.command()
 @click.option('--dumpfile', help='the file dump path to import')
-@click.option('--limit', default=100, help="pass a limit if you want to import just some files")
+@click.option('--limit', default=None, help="pass a limit if you want to import just some files")
 def import_open_library(dumpfile, limit):
-    data = pd.read_csv(dumpfile, sep='      ')
-    data.head()
+
+    limit = limit if limit is None else int(limit)
+    data = pd.read_csv(dumpfile, delimiter='	', header=None)
+
+    for i, entity in enumerate(data[4].tolist()):
+        if limit and i >= limit:
+            break
+
+        book = Book.from_dict(json.loads(entity))
+        print(book.to_dict())
 
 
 if __name__ == '__main__':
--- okreads/book.py
+++ okreads/book.py
@@ -4,5 +4,9 @@ class Book:
         self.title = title
         self.author = author
 
+    @staticmethod
+    def from_dict(data: dict):
+        return Book('', data['title'], data['authors'][0]['author']['key'])
+
     def to_dict(self):
         return {'isbn': self.isbn, 'title': self.title, 'author': self.author}

82dd9eca43889ba9ce9e59b5abcc8a2d952c59a5
JeanCarloMachado <contato@jeancarlomachado.com.br>
2019-10-19 18:02:08 +0200

    be able to import author

--- okreads/author.py
+++ okreads/author.py
@@ -1,15 +1,25 @@
 from okreads.db import Db
+from typing import Optional
 import json
 
 
 class Author:
-    def __init__(self, name, bio, location, birth_date, death_date, wikipedia_link, open_library_id):
+    def __init__(self,
+                 name="Unknown",
+                 bio=None,
+                 location=None,
+                 birth_date=None,
+                 death_date=None,
+                 wikipedia_link=None,
+                 id=None,
+                 open_library_id=None):
         self.name = name
         self.bio = bio
         self.location = location
         self.birth_date = birth_date
         self.death_date = death_date
         self.wikipedia_link = wikipedia_link
+        self.id = id
         self.open_library_id = open_library_id
 
     def to_dict(self):
@@ -17,13 +27,39 @@ class Author:
 
 
 class AuthorRepository:
-    def save(self, author: Author):
-        Db.execute('insert into author (info, open_library_id ) values (:info, :open_library_id)',
-                   {"info": json.dumps(author.to_dict()), "open_library_id": author.open_library_id})
+    def create(self, author: Author):
+        Db.execute('insert into author (info, open_library_id ) values (:info, :open_library_id)', {
+            "info": json.dumps(author.to_dict()),
+            "open_library_id": author.open_library_id
+        })
+
+    def update(self, author: Author):
+        Db.execute('update author set info = (:info) where open_library_id = :open_library_id', {
+            "info": json.dumps(author.to_dict()),
+            "open_library_id": author.open_library_id
+        })
 
     def getById(self, id) -> Author:
-        author = Db.fetchAll(f'SELECT * FROM author where id={id};')
-        return self._mapToEntity(author.first()['info'])
+        author = Db.fetchAll(f'SELECT * FROM author where id=:id;', {'id': id})
+        return self._mapToEntity(author.first())
+
+    def findByOpenLibraryId(self, id) -> Optional[Author]:
+        author = Db.fetchAll(
+            f'SELECT id, info, open_library_id FROM author where open_library_id=:id;', {'id': id})
+        data = author.first()
+        if data is None:
+            return None
+
+        return self._mapToEntity(data)
 
     def _mapToEntity(self, data):
-        return Author(name=data["name"], bio=data["bio"], location=data["location"], birth_date=data['birth_date'], death_date=data['death_date'], wikipedia_link=data['wikipedia_link'], open_library_id=data['open_library_id'])
+        author = Author(name=data['info']["name"],
+                        bio=data['info']["bio"],
+                        location=data['info']["location"],
+                        birth_date=data['info']['birth_date'],
+                        death_date=data['info']['death_date'],
+                        wikipedia_link=data['info']['wikipedia_link'],
+                        id=data['id'],
+                        open_library_id=data['open_library_id'])
+
+        return author
